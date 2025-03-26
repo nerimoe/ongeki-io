f86de99fbe835019ad0499749028e73c4121c6b8
akachan <i@neri.moe>
2025-03-27 02:05:21 +0800

    实现mu3_io_led_init

--- MU3Input/MU3IO.cs
+++ MU3Input/MU3IO.cs
@@ -110,6 +110,9 @@ namespace MU3Input
 
             pos = IO.Lever;
         }
+        
+        [DllExport(CallingConvention.Cdecl), ExportName = "mu3_io_led_init"]
+        public static void InitLed() {}
 
         [DllExport(CallingConvention.Cdecl, ExportName = "mu3_io_set_led")]
         public static void SetLed(uint data)
